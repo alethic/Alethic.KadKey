01147f5af4c56ce4886c297cb4ed7ebca843b8dc
Jerome Haltom <jhaltom@alethic.solutions>
2020-11-06 13:50:19 -0600

    Rename project.

---
+++ Alethic.KeyShift.Console/Controllers/KademliaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Cogito.Kademlia;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alethic.KadKey.Console.Controllers
+{
+
+    [Route("kademlia")]
+    [ApiController]
+    public class KademliaController : ControllerBase
+    {
+
+        readonly IKRouter<KNodeId256> router;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="router"></param>
+        public KademliaController(IKRouter<KNodeId256> router)
+        {
+            this.router = router ?? throw new ArgumentNullException(nameof(router));
+        }
+
+        [HttpGet]
+        public string Foo() => "hui";
+
+        [HttpGet("peers")]
+        public IAsyncEnumerable<object> GetPeersAsync()
+        {
+            return router.AsAsyncEnumerable().Select(i => new
+            {
+                NodeId = i.Id,
+                Endpoints = i.Endpoints.Select(j => j.ToUri())
+            });
+        }
+
+    }
+
+}
---
+++ Alethic.KeyShift.Console/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Tasks;
+
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+
+using Cogito.Autofac;
+using Cogito.Autofac.DependencyInjection;
+using Cogito.Extensions.Options.Autofac;
+using Cogito.Kademlia;
+using Cogito.Kademlia.InMemory;
+using Cogito.Kademlia.Network.Udp;
+using Cogito.Kademlia.Protobuf;
+using Cogito.Serilog;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Alethic.KadKey.Console
+{
+
+    public static class Program
+    {
+
+        [RegisterAs(typeof(ILoggerConfigurator))]
+        public class LoggerConfigurator : ILoggerConfigurator
+        {
+
+            public global::Serilog.LoggerConfiguration Apply(global::Serilog.LoggerConfiguration configuration)
+            {
+                return configuration.MinimumLevel.Verbose();
+            }
+
+        }
+
+        static void RegisterKademlia(ContainerBuilder builder, ulong network)
+        {
+            builder.RegisterType<KProtobufMessageFormat<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KRefresher<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KConnector<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KInvoker<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KInvokerPolicy<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KRequestHandler<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KFixedTableRouter<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KNodeLookup<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KValueLookup<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KInMemoryStore<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KInMemoryPublisher<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KEngine<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KUdpProtocol<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<KUdpMulticastDiscovery<KNodeId256>>().AsImplementedInterfaces().SingleInstance();
+            builder.Configure<KEngineOptions<KNodeId256>>(o => o.NodeId = KNodeId<KNodeId256>.Create());
+            builder.Configure<KFixedTableRouterOptions<KNodeId256>>(o => { });
+            builder.Configure<KUdpOptions<KNodeId256>>(o => { o.Network = network; });
+        }
+
+        /// <summary>
+        /// Main application entry point.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static async Task Main(string[] args) =>
+            await Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory(b => { b.RegisterAllAssemblyModules(); RegisterKademlia(b, 40512); b.Populate(s => s.AddMvc().AddApplicationPart(typeof(Program).Assembly).AddControllersAsServices()); }))
+                .ConfigureWebHostDefaults(c => c.Configure(c => c.UseRouting().UseEndpoints(e => { e.MapControllers(); e.Map("/", async context => await context.Response.WriteAsync(Newtonsoft.Json.JsonConvert.SerializeObject(context.RequestServices.GetService<IActionDescriptorCollectionProvider>().ActionDescriptors))); })))
+                .RunConsoleAsync();
+
+    }
+
+}
